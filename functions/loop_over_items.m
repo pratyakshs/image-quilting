151e4392bf35ef68b620b31a38ddfa4514350d77
Manik Dhar <manik@cse.iitb.ac.in>
2015-11-12 23:09:08 +0530

    Added main loop

--- functions/loop_over_items.m
+++ functions/loop_over_items.m
@@ -1,7 +1,11 @@
-function [ output_args ] = loop_over_items( input_args )
-%LOOP_OVER_ITEMS Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [ out_im ] = loop_over_items( Im, norm_func, diff_func, patch_size, overlap_size, out_size )
+%LOOP_OVER_ITEMS
+    out_im = -1 * ones(out_size);
+    for i = 1:(patch_size(1)-overlap_size(1)):out_size(1)
+        for j = 1:(patch_size(2)-overlap_size(2)):out_size(2)
+            patch = get_quilt_patch(out_im(i:(i + patch_size(1)), j:(j + patch_size(2))), Im, norm_func, diff_func, patch_size);
+            out_im(i:(i + patch_size(1)), j:(j + patch_size(2))) = patch;
+        end
+    end
 end
 
