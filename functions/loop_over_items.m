12df307f114d11b8f989e9fd26e1c25631d83ba8
Manik Dhar <manik@cse.iitb.ac.in>
2015-11-12 23:41:11 +0530

    Added random_patcher and changed things in main loop

--- functions/loop_over_items.m
+++ functions/loop_over_items.m
@@ -3,8 +3,10 @@ function [ out_im ] = loop_over_items( Im, norm_func, diff_func, patch_size, ove
     out_im = -1 * ones(out_size);
     for i = 1:(patch_size(1)-overlap_size(1)):out_size(1)
         for j = 1:(patch_size(2)-overlap_size(2)):out_size(2)
-            patch = get_quilt_patch(out_im(i:(i + patch_size(1)), j:(j + patch_size(2))), Im, norm_func, diff_func, patch_size);
-            out_im(i:(i + patch_size(1)), j:(j + patch_size(2))) = patch;
+            r_i = min((i + patch_size(1) - 1), out_size(1));
+            c_j = min((j + patch_size(2) - 1), out_size(2));
+            patch = get_quilt_patch(out_im(i:r_i, j:c_j), Im, norm_func, diff_func, patch_size);
+            out_im(i:c_i, j:c_j) = patch;
         end
     end
 end
