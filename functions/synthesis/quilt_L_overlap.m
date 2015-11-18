5d14b0ffefd731e114b5e7fbe305b75d60b969c0
Manik Dhar <manik@cse.iitb.ac.in>
2015-11-18 23:01:38 +0530

    Fixed bugs (it is working!)

--- functions/synthesis/quilt_L_overlap.m
+++ functions/synthesis/quilt_L_overlap.m
@@ -12,20 +12,23 @@ function [ best_patch ] = quilt_L_overlap(overlap_top, overlap_left, Im, patch_s
         patch = Im(r_take:r_take+patch_size(1)-1, c_take:c_take+patch_size(2)-1, :);
         overlap_error_top = single(patch(1:overlap_top_size(1), :, :)) - single(overlap_top);
         overlap_error_left = single(patch(:, 1:overlap_left_size(2), :)) - single(overlap_left);
-        sum = 0;
-        for j = 1:patch_size(1)
-            for k = 1:patch_size(2)
-                v1 = norm(reshape(single(target(j, k, :)), [3 1]));
-                v2 = norm(reshape(single(patch(j, k, :)), [3 1]));
-                temp = (v1 - v2)^2;
-                if mode == 1
-                    v3 = norm(reshape(single(send_t(j, k, :)), [3 1]));
-                    temp = temp * beta + (1 - beta) * (v3 - v2)^2;
-                end
-                sum = sum + temp;
-            end
-        end
-        l2_error = alpha * norm([norm(overlap_error_top(:)), norm(overlap_error_left(:))]) + (1 - alpha) * sum;
+        %sum = 0;
+        V1 = mean(single(target), 3);
+        V2 = mean(single(patch), 3);
+        corr_err = sum(sum((V1 - V2).^2));
+        %for j = 1:patch_size(1)
+        %    for k = 1:patch_size(2)
+        %        v1 = norm(reshape(single(target(j, k, :)), [3 1]));
+        %        v2 = norm(reshape(single(patch(j, k, :)), [3 1]));
+        %        temp = (v1 - v2)^2;
+        %        if mode == 1
+        %            v3 = norm(reshape(single(send_t(j, k, :)), [3 1]));
+        %            temp = temp * beta + (1 - beta) * (v3 - v2)^2;
+        %        end
+        %        sum = sum + temp;
+        %    end
+        %end
+        l2_error = alpha * norm([norm(overlap_error_top(:)), norm(overlap_error_left(:))]) + (1 - alpha) * corr_err;
         if l2_error < best_error
             best_error = l2_error;
             best_patch = patch;
--- functions/synthesis/quilt_top_overlap.m
+++ functions/synthesis/quilt_top_overlap.m
@@ -10,20 +10,23 @@ function [ best_patch ] = quilt_top_overlap(overlap, Im, patch_size, iters, targ
         c_take = randi([1, c - patch_size(2) + 1], 1, 1);
         patch = Im(r_take:r_take+patch_size(1)-1, c_take:c_take+patch_size(2)-1, :);
         overlap_error = single(patch(1:overlap_size(1), :, :)) - single(overlap);
-        sum = 0;
-        for j = 1:patch_size(1)
-            for k = 1:patch_size(2)
-                v1 = norm(reshape(single(target(j, k, :)), [3 1]));
-                v2 = norm(reshape(single(patch(j, k, :)), [3 1]));
-                temp = (v1 - v2)^2;
-                if mode == 1
-                    v3 = norm(reshape(single(send_t(j, k, :)), [3 1]));
-                    temp = temp * beta + (1 - beta) * (v3 - v2)^2;
-                end
-                sum = sum + temp;
-            end
-        end
-        l2_error = alpha * norm(overlap_error(:)) + (1 - alpha) * sum;
+        %sum = 0;
+        V1 = mean(single(target), 3);
+        V2 = mean(single(patch), 3);
+        corr_err = sum(sum((V1 - V2).^2));
+        %for j = 1:patch_size(1)
+        %    for k = 1:patch_size(2)
+        %        v1 = norm(reshape(single(target(j, k, :)), [3 1]));
+        %        v2 = norm(reshape(single(patch(j, k, :)), [3 1]));
+        %        temp = (v1 - v2)^2;
+        %        if mode == 1
+        %            v3 = norm(reshape(single(send_t(j, k, :)), [3 1]));
+        %            temp = temp * beta + (1 - beta) * (v3 - v2)^2;
+        %        end
+        %        sum = sum + temp;
+        %    end
+        %end
+        l2_error = alpha * norm(overlap_error(:)) + (1 - alpha) * corr_err;
         if l2_error < best_error
             best_error = l2_error;
             best_patch = patch;
